25a70e434680ab843e475b0689165b2d72d6b775
ardlee <ardlee@ucsc.edu>
2025-02-03 13:06:20 -0800

    test finish

--- src/App.js
+++ src/App.js
@@ -17,9 +17,100 @@ export default function App() {
       description: '2/6/2023'
     },
     {
-      original: "https://picsum.photos/id/1019/1000/600/",
-      thumbnail: "https://picsum.photos/id/1019/250/150/"
-    }
+      original: "images/3-4-23.jpg",
+      thumbnail: "images/3-4-23.jpg",
+      description: '3/4/23'
+    },
+    {
+      original: "images/3-5-23.jpg",
+      thumbnail: "images/3-5-23.jpg", 
+      description: '3/5/2023'
+    },
+    {
+      original: "images/3-10-23.png",
+      thumbnail: "images/3-10-23.png", 
+      description: '3/10/2023'
+    },
+    {
+      original: "images/3-14-23.jpg",
+      thumbnail: "images/3-14-23.jpg", 
+      description: '3/14/2023'
+    },
+    {
+      original: "images/6-6-23.jpg",
+      thumbnail: "images/6-6-23.jpg", 
+      description: '6/6/2023'
+    },
+    {
+      original: "images/7-6-23.jpg",
+      thumbnail: "images/7-6-23.jpg", 
+      description: '7/6/2023'
+    },
+    {
+      original: "images/8-6-23.jpg",
+      thumbnail: "images/8-6-23.jpg", 
+      description: '8/6/2023'
+    },
+    {
+      original: "images/9-23-23.jpg",
+      thumbnail: "images/9-23-23.jpg", 
+      description: '9/23/2023'
+    },
+    {
+      original: "images/10-21-23.jpg",
+      thumbnail: "images/10-21-23.jpg", 
+      description: '10/21/2023'
+    },
+    {
+      original: "images/10-21-23-2.jpg",
+      thumbnail: "images/10-21-23-2.jpg", 
+      description: '10/21/2023'
+    },
+    {
+      original: "images/12-30-23.jpg",
+      thumbnail: "images/12-30-23.jpg", 
+      description: '12/30/2023'
+    },
+    {
+      original: "images/2-5-24.jpg",
+      thumbnail: "images/2-5-24.jpg", 
+      description: '2/5/2024'
+    },
+    {
+      original: "images/4-1-24.jpg",
+      thumbnail: "images/4-1-24.jpg", 
+      description: '4/1/2024'
+    },
+    {
+      original: "images/4-21-24.jpg",
+      thumbnail: "images/4-21-24.jpg", 
+      description: '4/21/2024'
+    },
+    {
+      original: "images/5-17-24.jpg",
+      thumbnail: "images/5-17-24.jpg", 
+      description: '5/17/2024'
+    },
+    {
+      original: "images/5-30-24.jpeg",
+      thumbnail: "images/5-30-24.jpeg", 
+      description: '5/30/2024'
+    },
+    {
+      original: "images/6-8-24.jpg",
+      thumbnail: "images/6-8-24.jpg", 
+      description: '6/8/2023'
+    },
+    {
+      original: "images/6-8-24-2.jpg",
+      thumbnail: "images/6-8-24-2.jpg", 
+      description: '6/8/2023'
+    },
+    {
+      original: "images/10-25-24.jpg",
+      thumbnail: "images/10-25-24.jpg", 
+      description: '10/25/2024'
+    },
   ];
   // Custom renderer for the currently displayed image
   const [currentDescription, setCurrentDescription] = useState(images[0].description);
