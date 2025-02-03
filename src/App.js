4d726d861649f0ff7d1414c274929736fe075bc8
ardlee <ardlee@ucsc.edu>
2025-02-02 22:23:06 -0800

    edits

--- src/App.js
+++ src/App.js
@@ -2,17 +2,19 @@
 import "./App.css";
 import ImageGallery from "react-image-gallery";
 import "react-image-gallery/styles/css/image-gallery.css";
+import { useState } from "react";
 
 export default function App() {
   const images = [
     {
-      original: "images/image1.png",
-      thumbnail: '/images/image1.png', 
-      description: 'Image 1 Description'
+      original: "images/image1.jpg",
+      thumbnail: '/images/image1.jpg', 
+      description: '2/5/2023'
     },
     {
-      original: "https://picsum.photos/id/1015/1000/600/",
-      thumbnail: "https://picsum.photos/id/1015/250/150/"
+      original: "images/image2.jpg",
+      thumbnail: '/images/image2.jpg', 
+      description: '2/6/2023'
     },
     {
       original: "https://picsum.photos/id/1019/1000/600/",
@@ -20,27 +22,25 @@ export default function App() {
     }
   ];
   // Custom renderer for the currently displayed image
-  const renderItem = (item) => {
-    return (
-      <div className="custom-image-container">
-        <img src={item.original} alt={item.description || "Gallery Image"} />
-        {item.description && <p className="image-description">{item.description}</p>}
-      </div>
-    );
-  };
+  const [currentDescription, setCurrentDescription] = useState(images[0].description);
 
   return (
     <div className="App">
-      <h1>Welcome to My Photo Gallery</h1>
+      <h1 className="image-description-header">{currentDescription}</h1>
+
       <ImageGallery
         items={images}
         showPlayButton={true}
         showFullscreenButton={true}
         slideInterval={5000}
         showIndex={true}
-        renderItem={renderItem} // Use custom renderer
+        onSlide={(index) => setCurrentDescription(images[index].description || "")} // Update description on slide change
+        renderItem={(item) => (
+          <div className="gallery-slide">
+            <img src={item.original} alt={item.description || "Gallery Image"} />
+          </div>
+        )}
       />
     </div>
-  );
-
+    );
 }
